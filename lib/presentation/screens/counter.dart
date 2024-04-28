0a46ef2a8ffe3118f354af59178ccf2bde28b7bd
erickdavestech <erickdavestech@gmail.com>
2024-04-28 08:33:41 -0400

    main.dart segunda_pantalla_Font,Style,Colum,FlActionbuttom

--- lib/presentation/screens/counter.dart
+++ lib/presentation/screens/counter.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 
 class Counter extends StatelessWidget {
   const Counter({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    return Scaffold(
+      body: const Center(
+        //Column recives multiple childs
+        child: Column(
+          // change colum direction
+          mainAxisAlignment: MainAxisAlignment.center,
+          //Here we can use , multiple widgets
+          children: [
+            Text(
+              '110',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+            Text(
+              'Clicks',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w100),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
       ),
     );
   }
