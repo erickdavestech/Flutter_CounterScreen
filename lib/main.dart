f9acc0479111cd9bd25a4c9a6a8edc8125d6ce4d
erickdavestech <erickdavestech@gmail.com>
2024-04-28 05:19:16 -0400

    main.dart segunda_pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_daves/presentation/screens/counter.dart';
 
 void main() {
-//Call MainApp using const for performance
   runApp(const MainApp());
 }
 
-//Class MainApp
 class MainApp extends StatelessWidget {
-  //Constructor
   const MainApp({super.key});
-// sobre-escribe los metodos
   @override
-  // returns a widget method name is build
   Widget build(BuildContext context) {
-    // Widget MaterialApp that will be returned
     return const MaterialApp(
-      // Delete the Debug sign "Banner is the name"
-      debugShowCheckedModeBanner: false,
-      // using Scaffold
-      home: Scaffold(
-        // Center the Scaffold
-        body: Center(
-          //
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false, home: Counter());
   }
 }
---
+++ lib/presentation/screens/counter.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatelessWidget {
+  const Counter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
